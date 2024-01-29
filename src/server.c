bd352d026609fcb2688a97a291fa6ea983d9a29f
justeozan <justeoz.an@gmail.com>
2024-01-29 18:26:09 +0100

    seperate basic from bonus

--- src/server.c
+++ src/server.c
@@ -6,13 +6,37 @@
 /*   By: ozasahin <ozasahin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:06:51 by ozasahin          #+#    #+#             */
-/*   Updated: 2024/01/29 18:14:35 by ozasahin         ###   ########.fr       */
+/*   Updated: 2024/01/29 18:23:55 by ozasahin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
+void	handler_signal(int signal, siginfo_t *info, void *context)
+{
+	static unsigned char	c = 0;
+	static int				bit = -1;
 
+	(void)context;
+	(void)info;
+	// if (kill(info->si_pid, 0) < 0)
+	// {
+	// 	ft_printf("ERROR : cant send signal to pid: %d\n", info->si_pid);
+	// 	exit(EXIT_FAILURE);
+	// }
+	if (bit < 0)
+		bit = __CHAR_BIT__ * sizeof(c) - 1;
+	if (signal == SIGUSR1)
+		c |= 1 << bit;
+	else if (signal == SIGUSR2)
+		c &= ~(1 << bit);
+	if (!bit && c)
+		write(1, &c, 1);
+	// else if (!bit && !c)
+	// 	kill(info->si_pid, SIGUSR2);
+	bit--;
+	// kill(info->si_pid, SIGUSR1);
+}
 
 int	main(void)
 {
